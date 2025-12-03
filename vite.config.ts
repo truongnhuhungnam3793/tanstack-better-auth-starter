b10583cd31e4a5dcfd06ceb3775d7585e5dfa8b5
HUNGNAMHOMEPC\truon <truongnhuhungnam@gmail.com>
2025-12-03 22:33:43 +0700

    feat: Initialize TanStack Better Auth Starter project with core structure, routing, and database integrations.

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import viteReact from '@vitejs/plugin-react'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+import neon from './neon-vite-plugin.ts'
+import { nitro } from 'nitro/vite'
+
+const config = defineConfig({
+  plugins: [
+    devtools(),
+    neon,
+    nitro(),
+    // this is the plugin that enables path aliases
+    viteTsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+  ],
+})
+
+export default config
